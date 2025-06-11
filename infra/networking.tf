5376d9176b2856db7bb62f95182a8c2623d69ec5
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-06-11 23:15:03 +0200

    Added resource_group as a data resource

--- infra/networking.tf
+++ infra/networking.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "example" {
   name                = "private-network"
-  resource_group_name = azurerm_resource_group.block_of_energy_rg.name
-  location            = azurerm_resource_group.block_of_energy_rg.location
+  resource_group_name = data.azurerm_resource_group.block_of_energy_rg.name
+  location            = data.azurerm_resource_group.block_of_energy_rg.location
   address_space       = ["10.0.0.0/16"]
 }
