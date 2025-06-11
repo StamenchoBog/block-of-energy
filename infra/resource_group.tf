30d98544231186a724dab525e0768afb541ac9b4
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-06-11 23:38:38 +0200

    Removed resource_groups.tf (#1)
    * Removed resource_groups.tf
    
    * Added resource_group as a data resource
    
    * Changed tofu_actions workflow
    
    * Added permissions for pull-requests for github bot
    
    * Changed working_dir for terraform_apply and formatted resource_group.tf
    
    * Changed naming of vnet
    
    * Added new line in tofu_actions.yaml

---
+++ infra/resource_group.tf
@@ -0,0 +1,3 @@
+data "azurerm_resource_group" "block_of_energy_rg" {
+  name = "block-of-energy"
+}
--- infra/resource_groups.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "block_of_energy_rg" {
-  name     = "block-of-energy"
-  location = "Germany West Central"
-}
