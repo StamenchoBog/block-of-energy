2444824bce8b406c82b16496ab9ece67efdae1f3
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-06-26 19:53:53 +0200

    Added key vaults, kv-general and kv-bchain

--- infra/key_vaults.tf
+++ infra/key_vaults.tf
@@ -1,1 +1,47 @@
+resource "azurerm_key_vault" "kv_general" {
+  name = "kv-general"
+  location = data.azurerm_resource_group.block_of_energy_rg.location
+  resource_group_name = data.azurerm_resource_group.block_of_energy_rg.name
+  enabled_for_disk_encryption = true
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 30
+  purge_protection_enabled = true
 
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "kv_general_access_policy" {
+  key_vault_id = azurerm_key_vault.kv_general.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+}
+
+resource "azurerm_key_vault" "kv_bchain" {
+  name                        = "kv-bchain"
+  location = data.azurerm_resource_group.block_of_energy_rg.location
+  resource_group_name = data.azurerm_resource_group.block_of_energy_rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 90
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "kv_bchain_access_policy" {
+  key_vault_id = azurerm_key_vault.kv_bchain.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+}
