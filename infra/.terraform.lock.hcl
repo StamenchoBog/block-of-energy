31e2ce9df37ecfaac56b6c0d884c9686abf04b0a
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-06-26 18:45:01 +0200

    Added subnets, and key vault (#2)
    * Removed resource_groups.tf
    
    * Added resource_group as a data resource
    
    * Changed tofu_actions workflow
    
    * Added permissions for pull-requests for github bot
    
    * Changed working_dir for terraform_apply and formatted resource_group.tf
    
    * Changed naming of vnet
    
    * Added new line in tofu_actions.yaml
    
    * Added subnets
    
    * Test changes in subnet
    
    * Fix for for_each loop not including all subnets
    
    * Fix for for_each loop not including all subnets
    
    * Fix for for_each loop not including all subnets
    
    * Fix for for_each loop not including all subnets

---
+++ infra/.terraform.lock.hcl
@@ -0,0 +1,35 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/hashicorp/azuread" {
+  version = "3.4.0"
+  hashes = [
+    "h1:CzPZi67nUjdNRg6O2IJMl5AR5Sx5/5tZwyNZyDHQwQw=",
+    "zh:1d281545e673788f15fc03cd7637465f14c03ce28492aed112928af88a1e6a91",
+    "zh:3cfd8de978d7e3de6ada15313ce26e61b90465e345078595ce2b677d071f29cd",
+    "zh:411634ad8b4fb69ac06e5464f08bab043230b9f18a9171032f3bfe1f2c050c5b",
+    "zh:4763b8852fe1613e5e6fc742cb12018369cfd46ed630da2b3d65809bfcdf58ea",
+    "zh:81df6d495f95a5e83529961f1e0e5dc53f90938f08c72ce88dd7bba35b5bbf89",
+    "zh:9681fc8bce2f5d5dd6dd7026d9a7cf37f2a8b5867f42c996129b8fe49833c5a7",
+    "zh:c4d36ef64e8d51b3bbfeb148559e7fd0663576b0c6a38333c95500752f6778c1",
+    "zh:dc3b4d7385a622a4fe9865d986e2b4f501678d4e709a66b350f0d584790aba7d",
+    "zh:f38af14a92027bfa688cb38c9ade8ed31c31c789333d426307ff7924626996c9",
+  ]
+}
+
+provider "registry.opentofu.org/hashicorp/azurerm" {
+  version     = "4.34.0"
+  constraints = "~> 4.1"
+  hashes = [
+    "h1:hub8PpZCKgjEIZPUzy21VgyNPRbba33k0x8silx+Fv4=",
+    "zh:0e362c495108fe37d7b04d637dc250d5ebf126ca5d90a10ad780aa67b7c753b9",
+    "zh:23f17ec8857909e9c0057e97331fa1b07c65b1245254c738e70aff7ca7cc7f8d",
+    "zh:3c94dc54c21e2c2732607c8e2437d189fbad125c8e6415b88ee0283a589da93c",
+    "zh:510c970417b0c61242da82b4e8f1ba57ba96cab7ed922e2a08bfaabc02292546",
+    "zh:8d3b2c82a6b9dd90cd9c3a1e445e12e64b43023756ccbb04cacccbac309781ed",
+    "zh:b0deda7eaf5ea61dcaa7f97b452871f97163bc6c97a3b6e8899e251856b871b5",
+    "zh:ba01c0d7c02fcac6922587850c663358abe019d89de7f723c0ecc5e2f57a83e6",
+    "zh:c60474d4a5cff6e3875d80af756d983506abca96432ba409633010b2063648f9",
+    "zh:e221826a1d113a277aeddfe68ac4444ebf016ce9d13b8d28ba71b7b50d22764a",
+  ]
+}
---
+++ infra/variables.tf
@@ -0,0 +1,54 @@
+variable "networking" {
+  type = any
+  default = {
+    vnet = {
+      name          = "private-vnet"
+      address_space = ["10.0.0.0/16"]
+    }
+    subnets = [
+      {
+        name             = "snet-aks"
+        description      = "Hosts the Kubernetes nodes and pods"
+        address_prefixes = ["10.0.0.0/22"]
+        create           = true
+        delegation       = []
+      },
+      {
+        name             = "snet-functions"
+        description      = "Dedicated subnet for VNet Integration of the internal Azure Function Apps"
+        address_prefixes = ["10.0.4.0/25"]
+        create           = true
+        delegation = [
+          {
+            name = "functions-delegation"
+            service_delegation = {
+              name    = "Microsoft.Web/serverFarms"
+              actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+            }
+          }
+        ]
+      },
+      {
+        name             = "snet-private-endpoints"
+        description      = "Houses the Private Endpoints for PaaS services (Azure Data Explorer, Storage Account, Key Vault, Container Registry)."
+        address_prefixes = ["10.0.4.128/26"]
+        create           = true
+        delegation       = []
+      },
+      {
+        name             = "snet-app-gateway"
+        description      = "Hosts the Azure Application Gateway. This is the only entry point for user traffic from the internet."
+        address_prefixes = ["10.0.5.0/26"]
+        create           = true
+        delegation       = []
+      },
+      {
+        name             = "snet-azure-firewall"
+        description      = "Hosts Azure Firewall to control all outbound traffic from your VNet to the internet."
+        address_prefixes = ["10.0.6.0/26"]
+        create           = true
+        delegation       = []
+      },
+    ]
+  }
+}
