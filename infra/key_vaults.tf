b9015038ed91834014d00b63dea94f964c34dc33
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-06-26 19:59:29 +0200

    Renamed kv_bchain to kv_blockchain

--- infra/key_vaults.tf
+++ infra/key_vaults.tf
@@ -24,8 +24,8 @@ resource "azurerm_key_vault_access_policy" "kv_general_access_policy" {
   ]
 }
 
-resource "azurerm_key_vault" "kv_bchain" {
-  name                        = "kv-bchain"
+resource "azurerm_key_vault" "kv_blockchain" {
+  name                        = "kv-blockchain"
   location = data.azurerm_resource_group.block_of_energy_rg.location
   resource_group_name = data.azurerm_resource_group.block_of_energy_rg.name
   enabled_for_disk_encryption = true
@@ -36,8 +36,8 @@ resource "azurerm_key_vault" "kv_bchain" {
   sku_name = "standard"
 }
 
-resource "azurerm_key_vault_access_policy" "kv_bchain_access_policy" {
-  key_vault_id = azurerm_key_vault.kv_bchain.id
+resource "azurerm_key_vault_access_policy" "kv_blockchain_access_policy" {
+  key_vault_id = azurerm_key_vault.kv_blockchain.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
