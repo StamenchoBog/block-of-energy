71bdb95994f5ad6b1185e3397b305d0494bc8a67
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-06-27 11:35:48 +0200

    Added key vaults (#5)
    * Removed resource_groups.tf
    
    * Added resource_group as a data resource
    
    * Changed tofu_actions workflow
    
    * Added permissions for pull-requests for github bot
    
    * Changed working_dir for terraform_apply and formatted resource_group.tf
    
    * Changed naming of vnet
    
    * Added new line in tofu_actions.yaml
    
    * Added subnets
    
    * Test changes in subnet
    
    * Fix for for_each loop not including all subnets
    
    * Fix for for_each loop not including all subnets
    
    * Fix for for_each loop not including all subnets
    
    * Fix for for_each loop not including all subnets
    
    * Added dependabot scanning and change tofu plan to be with colors
    
    * Reverted tofu plan to not use colours
    
    * Added empty end line
    
    * Removed .github/README.md
    
    * Added key vaults, kv-general and kv-bchain
    
    * Renamed kv_bchain to kv_blockchain
    
    * Formatted terraform files
    
    * Added .idea folder to be ignore in .gitignore
    
    * Added purge_protection for kv_blockchain

--- infra/key_vaults.tf
+++ infra/key_vaults.tf
@@ -1,1 +1,48 @@
+resource "azurerm_key_vault" "kv_general" {
+  name                        = "kv-general"
+  location                    = data.azurerm_resource_group.block_of_energy_rg.location
+  resource_group_name         = data.azurerm_resource_group.block_of_energy_rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 30
+  purge_protection_enabled    = true
 
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "kv_general_access_policy" {
+  key_vault_id = azurerm_key_vault.kv_general.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+}
+
+resource "azurerm_key_vault" "kv_blockchain" {
+  name                        = "kv-blockchain"
+  location                    = data.azurerm_resource_group.block_of_energy_rg.location
+  resource_group_name         = data.azurerm_resource_group.block_of_energy_rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 90
+  purge_protection_enabled    = true
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "kv_blockchain_access_policy" {
+  key_vault_id = azurerm_key_vault.kv_blockchain.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+  secret_permissions = []
+}
