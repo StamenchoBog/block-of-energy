4c6439a27fbcb956c6e737d21fb8cbf6e8afc834
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-06-29 19:46:38 +0200

    Infra (#16)
    * Added certificate_permissions to the kv_general
    
    * Added other certificate permissions in kv_general
    
    * Added other permissions to general KV
    
    * Fixed permissions causing errors
    
    * Fixed typo
    
    * Changed DPS service parameters
    
    * Removed hostname from iot_hub_dps
    
    * Added service_connect to iot_hub_access_policy
    
    * Changed IoT Hub to be from type B1 instead of F1

--- infra/iot.tf
+++ infra/iot.tf
@@ -7,17 +7,13 @@ resource "azurerm_iothub" "iot_hub" {
   min_tls_version              = "1.2"
 
   sku {
-    name     = "F1"
+    name     = "B1"
     capacity = "1"
   }
 
-  # TODO: Configure the Azure Modifier Function as endpoint and route
-  #endpoint {
-  #}
-
-  #route{
-
-  #}
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
 resource "azurerm_iothub_shared_access_policy" "iot_hub_access_policy" {
@@ -28,13 +24,15 @@ resource "azurerm_iothub_shared_access_policy" "iot_hub_access_policy" {
   registry_read   = true
   registry_write  = true
   service_connect = true
+  device_connect  = true
 }
 
 resource "azurerm_iothub_dps" "iot_hub_dps" {
-  name                = "${var.prefix}-iot-hub-dps"
-  resource_group_name = data.azurerm_resource_group.block_of_energy_rg.name
-  location            = data.azurerm_resource_group.block_of_energy_rg.location
-  allocation_policy   = "Hashed"
+  name                          = "${var.prefix}-iot-hub-dps"
+  resource_group_name           = data.azurerm_resource_group.block_of_energy_rg.name
+  location                      = data.azurerm_resource_group.block_of_energy_rg.location
+  allocation_policy             = "Hashed"
+  public_network_access_enabled = true
 
   sku {
     name     = "S1"
@@ -47,5 +45,25 @@ resource "azurerm_iothub_dps" "iot_hub_dps" {
     allocation_weight       = 150
     apply_allocation_policy = true
   }
+
+  depends_on = [
+    azurerm_iothub.iot_hub,
+    azurerm_iothub_shared_access_policy.iot_hub_access_policy
+  ]
+}
+
+resource "azurerm_iothub_dps_shared_access_policy" "example" {
+  name                = "${var.prefix}-iot-hub-dps-access-policy"
+  resource_group_name = data.azurerm_resource_group.block_of_energy_rg.name
+  iothub_dps_name     = azurerm_iothub_dps.iot_hub_dps.name
+
+  enrollment_write = true
+  enrollment_read  = true
+
+  depends_on = [
+    azurerm_iothub.iot_hub,
+    azurerm_iothub_shared_access_policy.iot_hub_access_policy,
+    azurerm_iothub_dps.iot_hub_dps
+  ]
 }
 
