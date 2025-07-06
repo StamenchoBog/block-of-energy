c8104c4d1a6dedc342eb134da9dba31a9f040e07
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-07-06 19:13:48 +0200

    Commented file cert.tf out

--- infra/cert.tf
+++ infra/cert.tf
@@ -1,46 +1,46 @@
-resource "azurerm_key_vault_certificate" "iot_hub_cert" {
-  name         = "${var.prefix}-iot-hub-client-cert"
-  key_vault_id = azurerm_key_vault.kv_general.id
-
-  certificate_policy {
-    issuer_parameters {
-      name = "Self"
-    }
-
-    secret_properties {
-      content_type = "application/x-pem-file"
-    }
-
-    key_properties {
-      exportable = true
-      key_type   = "RSA"
-      key_size   = 4096
-      reuse_key  = false
-    }
-
-    lifetime_action {
-      action {
-        action_type = "AutoRenew"
-      }
-
-      trigger {
-        lifetime_percentage = 80
-      }
-    }
-
-    x509_certificate_properties {
-      subject            = "CN=smarthome-mosquitto-gw"
-      validity_in_months = 12
-      extended_key_usage = [
-        "1.3.6.1.5.5.7.3.2"
-      ]
-      key_usage = [
-        "digitalSignature",
-        "keyEncipherment",
-      ]
-      subject_alternative_names {
-        dns_names = []
-      }
-    }
-  }
-}
+# resource "azurerm_key_vault_certificate" "iot_hub_cert" {
+#   name         = "${var.prefix}-iot-hub-client-cert"
+#   key_vault_id = azurerm_key_vault.kv_general.id
+#
+#   certificate_policy {
+#     issuer_parameters {
+#       name = "Self"
+#     }
+#
+#     secret_properties {
+#       content_type = "application/x-pem-file"
+#     }
+#
+#     key_properties {
+#       exportable = true
+#       key_type   = "RSA"
+#       key_size   = 4096
+#       reuse_key  = false
+#     }
+#
+#     lifetime_action {
+#       action {
+#         action_type = "AutoRenew"
+#       }
+#
+#       trigger {
+#         lifetime_percentage = 80
+#       }
+#     }
+#
+#     x509_certificate_properties {
+#       subject            = "CN=smarthome-mosquitto-gw"
+#       validity_in_months = 12
+#       extended_key_usage = [
+#         "1.3.6.1.5.5.7.3.2"
+#       ]
+#       key_usage = [
+#         "digitalSignature",
+#         "keyEncipherment",
+#       ]
+#       subject_alternative_names {
+#         dns_names = []
+#       }
+#     }
+#   }
+# }
