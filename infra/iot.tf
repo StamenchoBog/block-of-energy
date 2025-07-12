a2e80e73c2e25e0411714c518e9b03d735d0da14
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-07-12 20:50:31 +0200

    Formatted files

--- infra/iot.tf
+++ infra/iot.tf
@@ -9,8 +9,8 @@ resource "azurerm_iothub" "iothub" {
   }
 
   local_authentication_enabled = true
-  event_hub_partition_count   = 2
-  event_hub_retention_in_days = 1
+  event_hub_partition_count    = 2
+  event_hub_retention_in_days  = 1
 
   tags = var.common_tags
 }
