805c434fc42402e1f1bb681737aa5a13c39c170a
Stamencho Bogdanovski <stamencho.bogdanovski@gmail.com>
2025-07-17 23:57:47 +0200

    Updated function

--- infra/functions.tf
+++ infra/functions.tf
@@ -15,6 +15,8 @@ resource "azurerm_linux_function_app" "azure_function_modifier" {
   storage_account_access_key = azurerm_storage_account.functions_storage.primary_access_key
   service_plan_id            = azurerm_service_plan.functions_service_plan.id
 
+  functions_extension_version = "~4"
+
   site_config {
     application_stack {
       python_version = "3.13"
@@ -23,10 +25,6 @@ resource "azurerm_linux_function_app" "azure_function_modifier" {
   }
 
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" : "python",
-    "FUNCTIONS_EXTENSION_VERSION" : "~4",
-    "WEBSITE_CONTENTOVERVNET" : "1",
-
     # IoT Hub and Service Bus configuration
     "IOT_HUB_NAME" : azurerm_iothub.iothub.name,
     "IOT_HUB_CONNECTION_STRING" : azurerm_iothub_shared_access_policy.iot_hub_access_policy_tofu.primary_connection_string,
